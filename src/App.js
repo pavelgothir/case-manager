60f1a2d81066ef4e249ee97d32c0b2efef225c7a
melny@LAPTOP-RCO4KM2G <foxiberation@gmail.com>
2022-06-25 18:14:12 +0300

    Next commit 2

--- src/App.js
+++ src/App.js
@@ -2,39 +2,19 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Cards from './components/Cards/Cards';
+import AddCase from './components/Cases/Add-case/AddCase';
 import Cases from './components/Cases/Cases';
 import Header from './components/Header/Header';
 
 
 function App() {
- /* const [t1, setT1] = useState('');
-  function clickHandler(){
-
-    fetch("http://case.ua/",{
-      method: "POST",
-      header: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body : JSON.stringify({action:1})
-    })
-    .then (response => response.text())
-    .then (response =>{
-      console.log(response);
-      setT1(response);
-    })
-  }
-  
-          <button onClick = {clickHandler}>Gogo</button>
-        <div>{t1}</div>
-  */
- 
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
         <div className='wrap__content'>
           <Routes>
-            <Route path='/add-case' element={<Cases />} />
+            <Route path='/add-case' element={<AddCase />} />
             <Route path='/cases' element={<Cases />} />
             <Route path='/cases' element={<Cases />} />
             <Route path='/cases' element={<Cases />} />
--- src/components/Cases/Add-case/AddCase.jsx
+++ src/components/Cases/Add-case/AddCase.jsx
@@ -1,9 +1,64 @@
 import React from "react";
+import PrimaryBtn from "../../Buttons/Primary";
+
+import s from "./AddCase.module.css";
+
+
 
 const AddCase = ()=>{
+    function checkData(){
+        console.log()
+    }
     return(
-        <div className="add__case">
+        <div className={s.add__case}>
+            <form className={s.form__add__case}>
+                <h1 className={s.header__text}>Додати кейс</h1>
+                <FormCaseInput type = "text" label="Назва кейсу"/>
+                <FormCaseInput type = "number" label="Телефон 1"/>
+                <FormCaseInput type = "number" label="Телефон 2"/>
+                <FormCaseInput type = "email" label="E-mail"/>
+                <FormCaseInput type = "text" label="Адреса"/>
+                
+                <div className={s.section__form}>
+                    <label className={s.label}>Канал комунікації</label>
+                    <select className={s.select}>
+                        <option>Viber</option>
+                        <option>Telegram</option>
+                        <option>Messenger</option>
+                        <option>WhatsApp</option>
+                        <option>Email</option>
+                    </select>
+                </div>
+                <FormCaseInput type = "text" label="Потреба"/>
+                <div className={s.section__form}>
+                    <label className={s.label}>Статус</label>
+                    <select className={s.select}>
+                        <option>Перший контакт</option>
+                        <option>У роботі</option>
+                        <option>Відмова</option>
+                        <option>Завершено</option>
+                        <option>Зупинено</option>
+                        <option>Передано у іншу організацію</option>
+                    </select>
+                </div>
+                <div>
+                    <PrimaryBtn name="Зберегти" click={test}/>
+                </div>
+            </form>
             
         </div>
     )
 }
+
+const FormCaseInput = (props)=>{
+    return (
+        <div className={s.section__form}>
+            <label className={s.label}>{props.label}</label>
+            <input type={props.type} className={s.input__text}/>
+        </div>
+    )
+}
+const test = function  (params) {
+    console.log("Hello")
+}
+export default AddCase;
